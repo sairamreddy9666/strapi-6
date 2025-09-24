be21167c0c2edd94541a1addf3b92a270987c874
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-24 15:37:56 +0530

    Delete strapi-task-4/strapi-task-4 directory

--- strapi-task-4/strapi-task-4/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "strapi_public_ip" {
-  value = aws_instance.strapi.public_ip
-}
-
-output "strapi_public_dns" {
-  value = aws_instance.strapi.public_dns
-}
-
--- strapi-task-4/strapi-task-4/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ami_id" {
-  default = "ami-01b6d88af12965bb6"
-}
