6b9f5ec137ea5b3ea60724326cf51250b1bb486c
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-25 21:42:59 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,14 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = ">= 4.0"
-}
-}
-required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  required_version = ">= 1.3.0"
 }
 
 
 provider "aws" {
-region = ap-south-1
+  region = "ap-south-1"
 }
