bf601eb0c8b1ae6651c00d7d0dacf7439bfd4f39
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-25 21:38:14 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "sairam-strapi-bucket"
-    region  = "ap-south-1"
-    key     = "ecs-fargate/terraform.tfstate"
-    encrypt = true
-  }
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source  = "hashicorp/aws"
-    }
+    bucket = "sairam-strapi-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
   }
 }
