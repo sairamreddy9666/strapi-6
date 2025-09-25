d5316d5359869254974bb6c408d8d12cada4c70c
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-25 17:53:26 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,3 @@
-output "alb_url" {
-description = "Public ALB DNS to access Strapi"
-value = aws_lb.alb.dns_name
-}
-
-
-output "ecr_repo_url" {
-value = aws_ecr_repository.strapi.repository_url
+output "lb_dns_name" {
+  value = aws_lb.LB.dns_name
 }
