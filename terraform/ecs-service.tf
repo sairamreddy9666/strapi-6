3540141a449d08efdd33d7ce94309ecb8b150386
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-08 21:23:33 +0530

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_service" "ECS-Service" {
   cluster                            = aws_ecs_cluster.ECS.id
   task_definition                    = aws_ecs_task_definition.TD.arn
   scheduling_strategy                = "REPLICA"
-  desired_count                      = 2
+  desired_count                      = 1
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   depends_on                         = [aws_alb_listener.Listener]
@@ -14,7 +14,7 @@ resource "aws_ecs_service" "ECS-Service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.TG.arn
     container_name   = "strapi-container"
-    container_port   = 80
+    container_port   = 1337
   }
 
 
