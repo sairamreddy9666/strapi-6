ff5cc9ac06e93f77113c428dcb80cbca880f524c
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-08 21:25:14 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,25 +1,3 @@
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "sairam-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
-}
-
-resource "aws_iam_role" "ecs_task_role" {
-  name               = "sairam-task-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
-}
-
-data "aws_iam_policy_document" "ecs_task_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+data "aws_iam_role" "ecs_task_exec_role" {
+  name = "ecsTaskExecutionRole"
 }
