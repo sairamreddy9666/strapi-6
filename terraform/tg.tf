da8f760d09293c59caabd3ad1088578b9b3cb1d7
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-08 21:26:41 +0530

    Update tg.tf

--- terraform/tg.tf
+++ terraform/tg.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "TG" {
   name        = "TG"
-  port        = "80"
+  port        = "1337"
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.vpc.id
