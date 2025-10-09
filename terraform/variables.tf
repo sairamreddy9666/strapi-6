d415f7fa08252fb0cc116ce2f9a9c7b511e43fb4
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-09 08:44:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,8 @@ variable "db_password" {
   type        = string
   default     = "Strapi123!"
 }
+
+variable "image_uri" {
+  description = "Docker image URI for Strapi container"
+  type        = string
+}
